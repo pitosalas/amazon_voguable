8a5049f415dd48f17d0ca77646f8270ec9582ffa
Pito Salas <pitosalas@gmail.com>
2011-06-28 16:03:32 -0400

    added support for ruby code gen

---
+++ atxr.rb
@@ -0,0 +1,195 @@
+#!/usr/bin/env ruby
+  require 'rubygems'
+  require 'asin'
+  require 'pp'
+  
+  include ASIN
+  
+  ASIN::Configuration.configure do |config|
+    config.secret = 'MDFS9VzmMO3TFbrBGOsRwUYq71+l5/ocSKVwz3Tt'
+    config.key = 'AKIAI3Z2GRNRZLLG7L4A'
+    config.logger = nil
+  end
+
+  include ASIN
+ def check_input
+  if ARGV.length == 0
+    puts "Run from command line as follows:"
+    puts "   ./at.rb <Browsenode> <brand> \"<keyword>, <keyword>,..........,<keyword>\""
+    exit
+  end
+ end
+  #==========================
+  #women apparel
+  h=Hash.new()
+  h["Dresses"]= 1045024 
+  h["Shoes"]= 672124011
+  h["Shorts"]=468556
+  h["Intimates"]=14333511
+  
+  h["Accessories"]=2474936011
+  h["Active3456051"]=
+  h["Blazers"]=1045112
+  h["Jackets"]=1045112
+  h["Dresses"]=1045024
+  h["Hoodies"]=1258603011
+  h["Intimates"]=14333511
+  h["Jeans"]=1048188
+  h["Jumpsuits"]=2381887011
+  h["Rompers"]=2381887011
+  h["Leggings"]=1258967011
+  h["Maternity"]=1285228011
+  h["Outerwear"]=1044646
+  h["Coats"]=1044646
+  h["Pants"]=1048184
+  h["Capris"]=1048184
+  h["Shorts"]=1048186
+  h["Skirts"]=1045022
+  h["Sleep"]=2376202011
+  h["Lounge"]=2376202011
+  h["Socks"]=1044886
+  h["Hosiery"]=1044886
+  h["Suits"]=1044460
+  h["Sweaters"]=1044456
+  h["Swim"]=1046622
+  h["Tops"]=2368343011
+  h["Tees"]=2368343011
+ #women shoes 
+  h["Athletic"]=679338011
+h["Boat Shoes"]=679379011
+h["Boots"]=679380011
+h["Bridal"]=679386011
+h["Costume"]=684422011
+h["Espadrilles"]=679391011
+h["Sneakers"]=679394011
+h["Flats"]=679399011
+h["Lace-Ups"]=679415011
+h["Slip-Ons"]=679404011
+h["Loafers"]=679404011
+h["Mules"]=679410011
+h["Clogs"]=679410011
+h["Outdoor"]=2368252011
+h["Pumps"]=679416011
+h["Sandals"]=679425011
+h["Slippers"]=679433011
+h["Sports]"]=679438011
+h["Footwear"]=679438011
+h["Work & Safety"]=679442011
+#jewelery
+h["Accessories"]=3888431
+h["Anklets"]=3885271
+h["Body Jewelry"]=3885201
+h["Bracelets & Bangles"]=3885251
+h["Brooches & Pins"]=3885491
+h["Charms"]=3885701
+h["Children's Jewelry"]=3880601
+h["Earrings"]=3885911
+h["Jewelry Sets"]=3898891
+h["Loose Gemstones"]=370742011
+h["Men's Jewelry"]=3887881
+h["Necklaces & Pendants"]=3886281
+h["Novelty Jewelry"]=2516784011
+h["Religious Jewelry"]=3893691
+h["Rings"]=3887251
+h["Wedding & Engagement Rings"]=16014541
+
+
+
+
+
+  
+  check_input
+  
+  browsenode = ARGV[0]
+#  puts h[browsenode.capitalize]
+  brand = ARGV[1]
+  keywords = ARGV[2]
+
+  if h.has_key?(browsenode.capitalize)  
+  list = search(:BrowseNode => h[browsenode.capitalize], :Brand => brand, :SearchIndex => :Apparel, :Keywords => keywords, :ResponseGroup => :Large)
+    puts "Searching for: BrowseNode: #{browsenode.capitalize}, brand: #{brand}, keywords: #{keywords}"
+  
+   
+   else
+   puts "Ooops! Please check your browse node and try again.."
+   check_input
+  end
+#  pp list[0]
+
+
+  
+# puts list[0].raw.ItemAttributes.Feature
+#  puts list[0].raw.ItemAttributes.Title
+#  puts list[0].raw.ItemAttributes.FabricType
+pp list[0].raw.ItemAttributes.ListPrice.Amount
+ 
+   counter = 1
+   puts "Outputting #{list.length} items"
+   list.each {
+   |item|
+      puts "Asin: #{item.asin} Item: #{counter}, name: #{item.title}, price: #{item.raw.ItemAttributes.price}\nimage: #{item.image_url}"
+      attribs = item.raw.ItemAttributes
+      tagfake = attribs.Feature + [attribs.Title] + [attribs.FabricType]
+      puts ">>> Brand: #{attribs.Brand}"
+      counter = counter + 1
+      puts "-----"
+  } unless true # list.nil
+  
+# Create or append to the output file
+  if false 
+    file = File.new("items.xml", "a")
+    list.each do |item|
+      file.puts "<item>"
+      file.puts "   <asin>#{item.asin}</asin>"
+      file.puts "   <title>#{item.title}</title>"
+      file.puts "   <price>#{item.raw.ItemAttributes.Price}</price>"
+      file.puts "   <image_url>#{item.image_url}</image_url>"
+
+      i_attrs = item.raw.ItemAttributes
+      file.puts "   <brand>#{i_attrs.Brand}</brand>"
+      file.puts "   <feature_list>"
+      features = i_attrs.Feature
+      features = [features] if features.class != Array
+      features.each do
+         |feature|
+         file.puts "       <feature>#{feature}</feature>"
+       end unless i_attrs.Feature.nil?
+      file.puts "   </feature_list>"
+      file.puts "   <fabric_type>#{i_attrs.FabricType}</fabric_type>"
+      file.puts "</item>"
+    end unless list.nil?
+    file.close
+end
+
+#Theme.create(:id => 1, :name=>'Lite', :background_color=>'0xC7FFD5', :title_text_color=>'0x222222',
+#                      :component_theme_color=>'0x001277', :carrier_select_color=>'0x7683FF', :label_text_color=>'0x000000',
+##                      :join_upper_gradient=>'0x6FAEFF', :join_lower_gradient=>'0x000000', :join_text_color=>'0xFFFFFF',
+#                      :cancel_link_color=>'0x001277', :border_color=>'0x888888', :carrier_text_color=>'0x000000', :public => true)
+
+  def esc(str)
+    str.sub( "'", %q{\\\'} ) unless str.nil?
+  end
+
+  file = File.new("items.rb", "a")
+  list.each do |item|
+      file.puts ("Item.create(")
+      file.puts ("  :asin => \'#{item.asin}\'")
+      file.puts ("  :name => \'#{esc(item.title)}\'")
+      file.puts ("  :image_url => \'#{esc(item.image_url)}\'")
+      
+      i_attrs = item.raw.ItemAttributes
+      features = i_attrs.Feature
+      features = [features] if features.class != Array
+      file.print ("   :features => \'")
+      features.each do
+         |feature|
+         file.print "#{esc(feature)}###"
+       end unless i_attrs.Feature.nil?
+      file.puts ("\'")
+      listprice = i_attrs.ListPrice
+      file.puts ("  :price => \'#{listprice.FormattedPrice}\'") unless listprice.nil?
+      file.puts ("  :brand => \'#{esc(i_attrs.Brand)}\'")
+      file.puts ("  :fabric_type => \'#{esc(i_attrs.FabricType)}\'")
+      file.puts (")")
+  end
+
---
+++ items.rb
@@ -0,0 +1,312 @@
+Item.create(
+  :asin => 'B004Q9SU9Y'
+  :name => 'Twelfth St  By Cynthia Vincent Women s Tiered Cami Maxi Dress'
+  :image_url => 'http   ecx images amazon com images I 31 2BIJmSTMwL jpg'
+   :features => 'Great for day or evening b###Great day to night maxi###'
+  :brand => 'Twelfth St  by Cynthia Vincent'
+  :fabric_type => '100  rayon'
+)Item.create(
+  :asin => 'B004VRJ3ZQ'
+  :name => 'Ella moss Women s Liberty One Shoulder Dress'
+  :image_url => 'http   ecx images amazon com images I 41NDMAcp 2BeL jpg'
+   :features => 'One shoulder###High low hem###'
+  :brand => 'Ella Moss'
+  :fabric_type => 'Self  96  rayon 4  spandex  contrast  50  modal 50  cotton'
+)Item.create(
+  :asin => 'B004XBD0LI'
+  :name => 'Testament Women s Starlight Kimono Dress'
+  :image_url => 'http   ecx images amazon com images I 41H1nKlPO4L jpg'
+   :features => 'Belted short dress###Made in usa###'
+  :brand => 'Testament'
+  :fabric_type => '100  rayon'
+)Item.create(
+  :asin => 'B004ZQL374'
+  :name => 'LACOSTE   MALANDRINO The Tube Top Dress'
+  :image_url => 'http   ecx images amazon com images I 31y 2BqoCUYgL jpg'
+   :features => ''
+  :brand => 'Lacoste'
+  :fabric_type => ''
+)Item.create(
+  :asin => 'B004WZQF12'
+  :name => 'BCBGMAXAZRIA Women s Koe Border Cuff Tunic Dress'
+  :image_url => 'http   ecx images amazon com images I 51Pj2aCkd L jpg'
+   :features => 'Rolled cuff sleeves###Classic fit###'
+  :brand => 'BCBGMAXAZRIA'
+  :fabric_type => '95  polyester 5  spandex'
+)Item.create(
+  :asin => 'B004PVTFR4'
+  :name => 'Twelfth St  By Cynthia Vincent Womens Shirred Corset Maxi Dress'
+  :image_url => 'http   ecx images amazon com images I 41FE97tiL1L jpg'
+   :features => 'Can be worn for an evening out or to a wedding###Flattering shape###'
+  :brand => 'Twelfth St  by Cynthia Vincent'
+  :fabric_type => '100  silk'
+)Item.create(
+  :asin => 'B004U9LW5Y'
+  :name => 'Dallin Chase Women s Mac Maxi Dress'
+  :image_url => 'http   ecx images amazon com images I 419QmnKmSsL jpg'
+   :features => 'This bustier top gown features an airy tank overlay at the bodice ###The colorful take on an animal print is light and pretty on silk ###'
+  :brand => 'Dallin Chase'
+  :fabric_type => '100  silk'
+)Item.create(
+  :asin => 'B004QTQ8C0'
+  :name => 'As U Wish Juniors Halter Long Dress'
+  :image_url => 'http   ecx images amazon com images I 41ZrPiIKkLL jpg'
+   :features => 'Maxi###Placement print###'
+  :brand => 'As U Wish'
+  :fabric_type => '100  rayon'
+)Item.create(
+  :asin => 'B0050AIYM6'
+  :name => 'aryn K Women s Floral One Shoulder Long Dress'
+  :image_url => 'http   ecx images amazon com images I 41Cg97LZDxL jpg'
+   :features => 'One shoulder asymmetrical floral###Long dress###'
+  :brand => 'aryn K'
+  :fabric_type => '100  Rayon'
+)Item.create(
+  :asin => 'B004XGWD9I'
+  :name => 'Joie Women s Brea Lace Dress'
+  :image_url => 'http   ecx images amazon com images I 41fmoN7VDnL jpg'
+   :features => 'Three quarter sleeves###Full lace body###'
+  :brand => 'Joie'
+  :fabric_type => '56  cotton 42  nylon 2  spandex'
+)Item.create(
+  :asin => 'B004Q9SU9Y'
+  :name => 'Twelfth St. By Cynthia Vincent Women\'s Tiered Cami Maxi Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/31%2BIJmSTMwL.jpg'
+   :features => 'Great for day or evening b###Great day to night maxi###'
+  :price => ''
+  :brand => 'Twelfth St. by Cynthia Vincent'
+  :fabric_type => '100% rayon'
+)
+Item.create(
+  :asin => 'B004VRJ3ZQ'
+  :name => 'Ella moss Women\'s Liberty One Shoulder Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41NDMAcp%2BeL.jpg'
+   :features => 'One shoulder###High low hem###'
+  :price => ''
+  :brand => 'Ella Moss'
+  :fabric_type => 'Self: 96% rayon/4% spandex; contrast: 50% modal/50% cotton'
+)
+Item.create(
+  :asin => 'B004XBD0LI'
+  :name => 'Testament Women\'s Starlight Kimono Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41H1nKlPO4L.jpg'
+   :features => 'Belted short dress###Made in usa###'
+  :price => ''
+  :brand => 'Testament'
+  :fabric_type => '100% rayon'
+)
+Item.create(
+  :asin => 'B004ZQL374'
+  :name => 'LACOSTE + MALANDRINO The Tube Top Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/31y%2BqoCUYgL.jpg'
+   :features => ''
+Item.create(
+  :asin => 'B004Q9SU9Y'
+  :name => 'Twelfth St. By Cynthia Vincent Women\'s Tiered Cami Maxi Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/31%2BIJmSTMwL.jpg'
+   :features => 'Great for day or evening b###Great day to night maxi###'
+  :price => '38500'
+  :brand => 'Twelfth St. by Cynthia Vincent'
+  :fabric_type => '100% rayon'
+)
+Item.create(
+  :asin => 'B004VRJ3ZQ'
+  :name => 'Ella moss Women\'s Liberty One Shoulder Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41NDMAcp%2BeL.jpg'
+   :features => 'One shoulder###High low hem###'
+  :price => '18400'
+  :brand => 'Ella Moss'
+  :fabric_type => 'Self: 96% rayon/4% spandex; contrast: 50% modal/50% cotton'
+)
+Item.create(
+  :asin => 'B004XBD0LI'
+  :name => 'Testament Women\'s Starlight Kimono Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41H1nKlPO4L.jpg'
+   :features => 'Belted short dress###Made in usa###'
+  :price => '11880'
+  :brand => 'Testament'
+  :fabric_type => '100% rayon'
+)
+Item.create(
+  :asin => 'B004ZQL374'
+  :name => 'LACOSTE + MALANDRINO The Tube Top Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/31y%2BqoCUYgL.jpg'
+   :features => ''
+Item.create(
+  :asin => 'B004Q9SU9Y'
+  :name => 'Twelfth St. By Cynthia Vincent Women\'s Tiered Cami Maxi Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/31%2BIJmSTMwL.jpg'
+   :features => 'Great for day or evening b###Great day to night maxi###'
+  :price => '38500'
+  :brand => 'Twelfth St. by Cynthia Vincent'
+  :fabric_type => '100% rayon'
+)
+Item.create(
+  :asin => 'B004VRJ3ZQ'
+  :name => 'Ella moss Women\'s Liberty One Shoulder Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41NDMAcp%2BeL.jpg'
+   :features => 'One shoulder###High low hem###'
+  :price => '18400'
+  :brand => 'Ella Moss'
+  :fabric_type => 'Self: 96% rayon/4% spandex; contrast: 50% modal/50% cotton'
+)
+Item.create(
+  :asin => 'B004XBD0LI'
+  :name => 'Testament Women\'s Starlight Kimono Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41H1nKlPO4L.jpg'
+   :features => 'Belted short dress###Made in usa###'
+  :price => '11880'
+  :brand => 'Testament'
+  :fabric_type => '100% rayon'
+)
+Item.create(
+  :asin => 'B004ZQL374'
+  :name => 'LACOSTE + MALANDRINO The Tube Top Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/31y%2BqoCUYgL.jpg'
+   :features => ''
+  :brand => 'Lacoste'
+  :fabric_type => ''
+)
+Item.create(
+  :asin => 'B004WZQF12'
+  :name => 'BCBGMAXAZRIA Women\'s Koe Border Cuff Tunic Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/51Pj2aCkd-L.jpg'
+   :features => 'Rolled cuff sleeves###Classic fit###'
+  :price => '13800'
+  :brand => 'BCBGMAXAZRIA'
+  :fabric_type => '95% polyester/5% spandex'
+)
+Item.create(
+  :asin => 'B004PVTFR4'
+  :name => 'Twelfth St. By Cynthia Vincent Womens Shirred Corset Maxi Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41FE97tiL1L.jpg'
+   :features => 'Can be worn for an evening out or to a wedding###Flattering shape###'
+  :price => '39600'
+  :brand => 'Twelfth St. by Cynthia Vincent'
+  :fabric_type => '100% silk'
+)
+Item.create(
+  :asin => 'B004U9LW5Y'
+  :name => 'Dallin Chase Women\'s Mac Maxi Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/419QmnKmSsL.jpg'
+   :features => 'This bustier top gown features an airy tank overlay at the bodice.###The colorful take on an animal print is light and pretty on silk.###'
+  :price => '27400'
+  :brand => 'Dallin Chase'
+  :fabric_type => '100% silk'
+)
+Item.create(
+  :asin => 'B004QTQ8C0'
+  :name => 'As U Wish Juniors Halter Long Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41ZrPiIKkLL.jpg'
+   :features => 'Maxi###Placement print###'
+  :price => '4400'
+  :brand => 'As U Wish'
+  :fabric_type => '100% rayon'
+)
+Item.create(
+  :asin => 'B0050AIYM6'
+  :name => 'aryn K Women\'s Floral One Shoulder Long Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41Cg97LZDxL.jpg'
+   :features => 'One shoulder asymmetrical floral###Long dress###'
+  :price => '8360'
+  :brand => 'aryn K'
+  :fabric_type => '100% Rayon'
+)
+Item.create(
+  :asin => 'B004XGWD9I'
+  :name => 'Joie Women\'s Brea Lace Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41fmoN7VDnL.jpg'
+   :features => 'Three-quarter sleeves###Full lace body###'
+  :price => '29800'
+  :brand => 'Joie'
+  :fabric_type => '56% cotton/42% nylon/2% spandex'
+)
+Item.create(
+  :asin => 'B004Q9SU9Y'
+  :name => 'Twelfth St. By Cynthia Vincent Women\'s Tiered Cami Maxi Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/31%2BIJmSTMwL.jpg'
+   :features => 'Great for day or evening b###Great day to night maxi###'
+  :price => '$385.00'
+  :brand => 'Twelfth St. by Cynthia Vincent'
+  :fabric_type => '100% rayon'
+)
+Item.create(
+  :asin => 'B004VRJ3ZQ'
+  :name => 'Ella moss Women\'s Liberty One Shoulder Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41NDMAcp%2BeL.jpg'
+   :features => 'One shoulder###High low hem###'
+  :price => '$184.00'
+  :brand => 'Ella Moss'
+  :fabric_type => 'Self: 96% rayon/4% spandex; contrast: 50% modal/50% cotton'
+)
+Item.create(
+  :asin => 'B004XBD0LI'
+  :name => 'Testament Women\'s Starlight Kimono Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41H1nKlPO4L.jpg'
+   :features => 'Belted short dress###Made in usa###'
+  :price => '$118.80'
+  :brand => 'Testament'
+  :fabric_type => '100% rayon'
+)
+Item.create(
+  :asin => 'B004ZQL374'
+  :name => 'LACOSTE + MALANDRINO The Tube Top Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/31y%2BqoCUYgL.jpg'
+   :features => ''
+  :brand => 'Lacoste'
+  :fabric_type => ''
+)
+Item.create(
+  :asin => 'B004WZQF12'
+  :name => 'BCBGMAXAZRIA Women\'s Koe Border Cuff Tunic Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/51Pj2aCkd-L.jpg'
+   :features => 'Rolled cuff sleeves###Classic fit###'
+  :price => '$138.00'
+  :brand => 'BCBGMAXAZRIA'
+  :fabric_type => '95% polyester/5% spandex'
+)
+Item.create(
+  :asin => 'B004PVTFR4'
+  :name => 'Twelfth St. By Cynthia Vincent Womens Shirred Corset Maxi Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41FE97tiL1L.jpg'
+   :features => 'Can be worn for an evening out or to a wedding###Flattering shape###'
+  :price => '$396.00'
+  :brand => 'Twelfth St. by Cynthia Vincent'
+  :fabric_type => '100% silk'
+)
+Item.create(
+  :asin => 'B004U9LW5Y'
+  :name => 'Dallin Chase Women\'s Mac Maxi Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/419QmnKmSsL.jpg'
+   :features => 'This bustier top gown features an airy tank overlay at the bodice.###The colorful take on an animal print is light and pretty on silk.###'
+  :price => '$274.00'
+  :brand => 'Dallin Chase'
+  :fabric_type => '100% silk'
+)
+Item.create(
+  :asin => 'B004QTQ8C0'
+  :name => 'As U Wish Juniors Halter Long Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41ZrPiIKkLL.jpg'
+   :features => 'Maxi###Placement print###'
+  :price => '$44.00'
+  :brand => 'As U Wish'
+  :fabric_type => '100% rayon'
+)
+Item.create(
+  :asin => 'B0050AIYM6'
+  :name => 'aryn K Women\'s Floral One Shoulder Long Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41Cg97LZDxL.jpg'
+   :features => 'One shoulder asymmetrical floral###Long dress###'
+  :price => '$83.60'
+  :brand => 'aryn K'
+  :fabric_type => '100% Rayon'
+)
+Item.create(
+  :asin => 'B004XGWD9I'
+  :name => 'Joie Women\'s Brea Lace Dress'
+  :image_url => 'http://ecx.images-amazon.com/images/I/41fmoN7VDnL.jpg'
+   :features => 'Three-quarter sleeves###Full lace body###'
+  :price => '$298.00'
+  :brand => 'Joie'
+  :fabric_type => '56% cotton/42% nylon/2% spandex'
+)
